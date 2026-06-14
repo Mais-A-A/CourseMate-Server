29e956fc6023d2d4170e753a58697e5be7bb6671
Hasan <hasansaafen1234@gmail.com>
2026-06-14 11:39:09 +0300

    add mais as admin

--- src/shared/utils/authUtils.ts
+++ src/shared/utils/authUtils.ts
@@ -23,7 +23,7 @@ export function detectRole(email: string): UserRole {
 
   if (isUniversityEmail(email)) {
     if (localPart == 'jabary980' || localPart == '221154') {
-      return 'supervisor'
+      return 'admin'
     } else if (/^\d+$/.test(localPart!)) {
       return 'student'
     }
